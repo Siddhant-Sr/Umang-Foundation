14e26c7709247c7a249e8501dcceeb8189d3f56e
Siddhant <siddhantsrivastava0811@gmail.com>
2025-12-29 19:20:39 +0530

    Color palette adjustment and homepage structure adjustment added scroll animation effect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Projects from './components/Projects';
-import Internship from './components/Internship';
-import StoryOfChange from './components/StoryOfChange';
-import BeTheChange from './components/BeTheChange';
-import MediaGallery from './components/MediaGallery';
-import PhotoGallery from './components/PhotoGallery';
-import CorporatePartners from './components/CorporatePartners';
-import Impact from './components/Impact';
-import GetInvolved from './components/GetInvolved';
-import SocialMedia from './components/SocialMedia';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import ServicesPage from './pages/ServicesPage';
+import ProjectsPage from './pages/ProjectsPage';
+import InternshipPage from './pages/InternshipPage';
+import StoryOfChangePage from './pages/StoryOfChangePage';
+import BeTheChangePage from './pages/BeTheChangePage';
+import MediaGalleryPage from './pages/MediaGalleryPage';
+import PhotoGalleryPage from './pages/PhotoGalleryPage';
+import ImpactPage from './pages/ImpactPage';
+import GetInvolvedPage from './pages/GetInvolvedPage';
+import SocialMediaPage from './pages/SocialMediaPage';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <About />
-      <Services />
-      <Projects />
-      <Internship />
-      <StoryOfChange />
-      <BeTheChange />
-      <MediaGallery />
-      <PhotoGallery />
-      <CorporatePartners />
-      <Impact />
-      <GetInvolved />
-      <SocialMedia />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/internship" element={<InternshipPage />} />
+          <Route path="/story-of-change" element={<StoryOfChangePage />} />
+          <Route path="/be-the-change" element={<BeTheChangePage />} />
+          <Route path="/media-gallery" element={<MediaGalleryPage />} />
+          <Route path="/photo-gallery" element={<PhotoGalleryPage />} />
+          <Route path="/impact" element={<ImpactPage />} />
+          <Route path="/contact-us" element={<GetInvolvedPage />} />
+          <Route path="/social-media" element={<SocialMediaPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import logo from '../assets/images/Umang-Foundation-Logo.png';
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
-          <h1>Umang Foundation</h1>
-          <p>Empowering Lives Through Education & Social Service</p>
+          <img src={logo} alt="Umang Foundation Logo" className="logo-image" />
         </div>
         <nav className="navbar">
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#internship">Internship</a></li>
-            <li><a href="#story-of-change">Story of Change</a></li>
-            <li><a href="#be-the-change">Be the Change</a></li>
-            <li><a href="#media-gallery">Media Gallery</a></li>
-            <li><a href="#photo-gallery">Photo Gallery</a></li>
-            <li><a href="#partners">Corporate Partners</a></li>
-            <li><a href="#social">Social Media</a></li>
-            <li><a href="#get-involved">Contact Us</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/internship">Internship</Link></li>
+            <li><Link to="/story-of-change">Story of Change</Link></li>
+            <li><Link to="/be-the-change">Be the Change</Link></li>
+            <li><Link to="/media-gallery">Media Gallery</Link></li>
+            <li><Link to="/photo-gallery">Photo Gallery</Link></li>
+            <li><Link to="/impact">Impact</Link></li>
+            <li><Link to="/contact-us">Contact Us</Link></li>
+            <li><Link to="/social-media">Social Media</Link></li>
           </ul>
         </nav>
       </div>
