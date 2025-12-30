a08e5ec843e3aabac9e6b2dc9ef6be72fe02b8e7
Siddhant <siddhantsrivastava0811@gmail.com>
2025-12-30 19:01:06 +0530

    changes in navbar names and about us sub pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import ServicesPage from './pages/ServicesPage';
+import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
 import InternshipPage from './pages/InternshipPage';
 import StoryOfChangePage from './pages/StoryOfChangePage';
 import BeTheChangePage from './pages/BeTheChangePage';
 import MediaGalleryPage from './pages/MediaGalleryPage';
 import PhotoGalleryPage from './pages/PhotoGalleryPage';
-import ImpactPage from './pages/ImpactPage';
+import CorporatePartnersPage from './pages/CorporatePartnersPage';
 import GetInvolvedPage from './pages/GetInvolvedPage';
 import SocialMediaPage from './pages/SocialMediaPage';
+import TrusteesProfilePage from './pages/TrusteesProfilePage';
+import GoverningBoardMemberPage from './pages/GoverningBoardMemberPage';
 
 function App() {
   return (
@@ -22,14 +24,16 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/trustees-profile" element={<TrusteesProfilePage />} />
+          <Route path="/governing-board-member" element={<GoverningBoardMemberPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/internship" element={<InternshipPage />} />
           <Route path="/story-of-change" element={<StoryOfChangePage />} />
           <Route path="/be-the-change" element={<BeTheChangePage />} />
           <Route path="/media-gallery" element={<MediaGalleryPage />} />
           <Route path="/photo-gallery" element={<PhotoGalleryPage />} />
-          <Route path="/impact" element={<ImpactPage />} />
+          <Route path="/corporate-partners" element={<CorporatePartnersPage />} />
           <Route path="/contact-us" element={<GetInvolvedPage />} />
           <Route path="/social-media" element={<SocialMediaPage />} />
         </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import logo from '../assets/images/Umang-Foundation-Logo.png';
 
 function Header() {
+  const [dropdownVisible, setDropdownVisible] = useState(null);
+
+  const handleMouseEnter = (menu) => {
+    setDropdownVisible(menu);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(null);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
@@ -13,16 +23,31 @@ function Header() {
         <nav className="navbar">
           <ul className="nav-links">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/services">Services</Link></li>
+            <li 
+              className="dropdown"
+              onMouseEnter={() => handleMouseEnter('about')}
+              onMouseLeave={handleMouseLeave}
+            >
+              <Link to="/about">
+                About Us
+                <span className="dropdown-arrow">▼</span>
+              </Link>
+              {dropdownVisible === 'about' && (
+                <ul className="dropdown-menu">
+                  <li><Link to="/trustees-profile">Trustees Profile</Link></li>
+                  <li><Link to="/governing-board-member">Governing Board Member</Link></li>
+                </ul>
+              )}
+            </li>
             <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/internship">Internship</Link></li>
             <li><Link to="/story-of-change">Story of Change</Link></li>
             <li><Link to="/be-the-change">Be the Change</Link></li>
             <li><Link to="/media-gallery">Media Gallery</Link></li>
             <li><Link to="/photo-gallery">Photo Gallery</Link></li>
-            <li><Link to="/impact">Impact</Link></li>
-            <li><Link to="/contact-us">Contact Us</Link></li>
+            <li><Link to="/corporate-partners">Corporate Partners</Link></li>
             <li><Link to="/social-media">Social Media</Link></li>
+            <li><Link to="/contact-us">Contact Us</Link></li>
           </ul>
         </nav>
       </div>
